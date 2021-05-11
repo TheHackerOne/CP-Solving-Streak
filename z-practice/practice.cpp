1af093d289cb210ecdfff42a6fb54a059212172a
TheHackerOne <yash2200@gmail.com>
2021-05-11 22:00:36 +0530

    Linekd List added

--- z-practice/practice.cpp
+++ z-practice/practice.cpp
@@ -1,9 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N = 1e5 + 7;
+int prime[N];
+
+void primeNumbers(int n) {
+	prime[1] = 0;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 2; j <= n / i; j++) {
+			int x = i * j;
+			prime[x] = 0;
+		}
+	}
+}
 
 int main() {
+	int n;
+	cin >> n;
+	memset(prime, -1, sizeof(prime));
 
-	string s = "";
-	cout << s.size() << endl;
+	primeNumbers(n);
+
+	for (int i = 1; i <= n; i++) {
+		if (prime[i] == -1)
+			cout << i << ", ";
+	}
+
 	return 0;
 }
