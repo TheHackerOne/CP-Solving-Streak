73c65d15a545d83cd55e85a790d9989812807e2e
TheHackerOne <yash2200@gmail.com>
2021-05-13 09:00:44 +0530

    count subset sum DP

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -4,69 +4,64 @@ const int N = 1e3 + 5;
 int dp[N][N];
 int dpTable[N][N];
 
-bool dpEqualSum(int *a, int size, int sum) {
-	//initialization
+int dpMethod(int *a, int size, int sum) {
 	for (int i = 0; i <= size; i++) {
 		for (int j = 0; j <= sum; j++) {
-			if (j == 0) dpTable[i][j] = 1;
-			else if (i == 0) dpTable[i][j] = 0;
+			if (j == 0) dp[i][j] = 1;
+			else if (i == 0) dp[i][j] = 0;
 		}
 	}
 	for (int i = 1; i <= size; i++) {
 		for (int j = 1; j <= sum; j++) {
-			if (a[i - 1] <= j) dpTable[i][j] = dpTable[i - 1][j] || dpTable[i - 1][j - a[i - 1]];
-			else dpTable[i][j] = dpTable[i - 1][j];
+			if (a[i - 1] <= j) dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]];
+			else dp[i][j] = dp[i - 1][j];
 		}
 	}
-	return dpTable[size][sum];
+	return dp[size][sum];
 }
 
-bool memoizedEqualSum(int *a, int size, int sum) {
-	if (sum == 0) return true;
-	if (size == 0) return false;
+int memoizeCountSubsetSums(int *a, int size, int sum) {
+	// base condition
+	if (sum == 0) return 1;
+	if (size == 0) return 0;
 	if (dp[size][sum] != -1)
 		return dp[size][sum];
 	if (a[size - 1] <= sum) {
-		return dp[size][sum] = memoizedEqualSum(a, size - 1, sum - a[size - 1]) || memoizedEqualSum(a, size - 1, sum);
+		dp[size][sum] = memoizeCountSubsetSums(a, size - 1, sum) + memoizeCountSubsetSums(a, size - 1, sum - a[size - 1]);
 	} else {
-		return dp[size][sum] = memoizedEqualSum(a, size - 1, sum);
+		dp[size][sum] = memoizeCountSubsetSums(a, size - 1, sum);
 	}
+	return dp[size][sum];
 }
 
-bool equalSumPartition(int *a, int size, int sum) {
-	if ((size == 0 && sum == 0) || (size != 0 && sum == 0)) return true;
-	if (size == 0) return false;
-
+int countSubsetSums(int *a, int size, int sum) {
+	// base condition
+	if (sum == 0) return 1;
+	if (size == 0) return 0;
+	int subsetCount = 0;
 	if (a[size - 1] <= sum) {
-		return equalSumPartition(a, size - 1, sum - a[size - 1]) || equalSumPartition(a, size - 1, sum);
+		subsetCount = countSubsetSums(a, size - 1, sum) + countSubsetSums(a, size - 1, sum - a[size - 1]);
 	} else {
-		return equalSumPartition(a, size - 1, sum);
+		subsetCount = countSubsetSums(a, size - 1, sum);
 	}
+	return subsetCount;
 }
 
 int main() {
 
-	int n, S = 0;
-	cin >> n ;
+	int n, S;
+	cin >> n >> S;
 	int a[n];
 	memset(dp, -1, sizeof(dp));
 	memset(dpTable, -1, sizeof(dp));
 
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
-		S += a[i];
 	}
 
-	cout << S << endl;
-	if ((S ^ 1) == 0) {
-		cout << 0 << endl;
-		cout << 0 << endl;
-		cout << 0 << endl;
-	} else {
-		cout << equalSumPartition(a, n, S / 2) << endl;
-		cout << memoizedEqualSum(a, n, S / 2) << endl;
-		cout << dpEqualSum(a, n, S / 2) << endl;
-	}
+	cout << countSubsetSums(a, n, S) << endl;
+	cout << memoizeCountSubsetSums(a, n, S) << endl;
+	cout << dpMethod(a, n, S) << endl;
 
 
 	return 0;
