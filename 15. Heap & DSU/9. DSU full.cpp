5beffda7bc2728583551111d4a8904beb85b2fea
TheHackerOne <yash2200@gmail.com>
2021-05-15 18:47:27 +0530

    DSU

--- 15. Heap & DSU/9. DSU full.cpp
+++ 15. Heap & DSU/9. DSU full.cpp
@@ -8,14 +8,26 @@ using namespace std;
 const int N = 1e3 + 4;
 int parent[N];
 
+// path compression
 int find(int a) {
-	if (a == parent[a])
+	if (parent[a] < 0)
 		return a;
-	else return find(parent[a]);
+	return parent[a] = find(parent[a]);
 }
 
-void union(int a, int b) {
-
+// union by rank
+void unionSet(int a, int b)
+{
+	if (parent[a] < parent[b])
+	{
+		parent[a] += parent[b];
+		parent[b] = a;
+	}
+	else
+	{
+		parent[b] += parent[a];
+		parent[a] = b;
+	}
 }
 
 int main() {
@@ -23,7 +35,12 @@ int main() {
 	int nodes, relations;
 	cin >> nodes >> relations;
 	for (int i = 0; i < relations; i++) {
-
+		int a, b;
+		cin >> a >> b;
+		a = find(a);
+		b = find(b);
+		if (a == b) continue;
+		else unionSet(a, b);
 	}
 
 	return 0;
