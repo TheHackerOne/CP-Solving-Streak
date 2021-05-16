bae1c8c48c3e1a80ae5727a4a14d8c9e273cc5cb
TheHackerOne <yash2200@gmail.com>
2021-05-16 10:57:18 +0530

    LCS all

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -4,63 +4,47 @@ const int N = 1e3 + 2;
 int dp[N][N];
 int dpTable[N][N];
 
-int dpPro(int *coins, int size, int S) {
-	for (int i = 0; i <= size; i++) {
-		for (int j = 0; j <= S; j++) {
-			if (j == 0) dpTable[i][j] = 0;
-			else dpTable[i][j] = INT_MAX - 1;
+int dpro(string s1, int size1, string s2, int size2) {
+	for (int i = 1; i <= size1; i++) {
+		for (int j = 1; j <= size2; j++) {
+			if (s1[i - 1] == s2[j - 1]) dpTable[i][j] = 1 + dpTable[i - 1][j - 1];
+			else dpTable[i][j] = max(dpTable[i - 1][j], dpTable[i][j - 1]);
 		}
 	}
-	for (int i = 1; i <= size; i++) {
-		for (int j = 1; j <= S; j++) {
-			if (coins[i - 1] <= S)
-				dpTable[i][j] = min(dpTable[i - 1][j], 1 + dpTable[i][j - coins[i - 1]]);
-			else
-				dpTable[i][j] = dpTable[i - 1][j];
+	for (int i = 1; i <= size1; i++) {
+		for (int j = 1; j <= size2; j++) {
+			cout << dpTable[i][j] << " ";
 		}
+		cout << endl;
 	}
-	return dpTable[size][S];
+	return dpTable[size1][size2];
 }
 
-int memoized(int *coins, int size, int S) {
-	if (S == 0) return 0;
-	if (size == 0) return INT_MAX;
-	if (dp[size][S] != -1) return dp[size][S];
-	if (coins[size - 1] <= S) {
-		int min1 = memoized(coins, size - 1, S);
-		int min2 = memoized(coins, size, S - coins[size - 1]);
-		if (min2 == INT_MAX)  min2--;
-		return dp[size][S] = min(min1, 1 + min2);
-	}
-	return dp[size][S] = memoized(coins, size - 1, S);
+int memoized(string s1, int size1, string s2, int size2) {
+	if (size1 == 0 || size2 == 0) return 0;
+	if (dp[size1][size2] != -1) return dp[size1][size2];
+	if (s1[size1 - 1] == s2[size2 - 1])
+		return dp[size1][size2] = 1 + memoized(s1, size1 - 1, s2, size2 - 1);
+	else
+		return dp[size1][size2] = max(memoized(s1, size1 - 1, s2, size2), memoized(s1, size1, s2, size2 - 1));
 }
 
-int coinChangeMinCoins(int *coins, int size, int S) {
-	if (S == 0) return 0;
-	if (size == 0) return INT_MAX;
-	if (coins[size - 1] <= S) {
-		int min1 = coinChangeMinCoins(coins, size - 1, S);
-		int min2 = coinChangeMinCoins(coins, size, S - coins[size - 1]);
-		if (min2 == INT_MAX)  min2--;
-		return min(min1, 1 + min2);
-	}
-	return coinChangeMinCoins(coins, size - 1, S);
+int LCS(string s1, int size1, string s2, int size2) {
+	if (size1 == 0 || size2 == 0) return 0;
+	if (s1[size1 - 1] == s2[size2 - 1])
+		return 1 + LCS(s1, size1 - 1, s2, size2 - 1);
+	else
+		return max(LCS(s1, size1 - 1, s2, size2), LCS(s1, size1, s2, size2 - 1));
 }
 
 int main() {
-	int n, S;
-	cin >> n >> S;
+	string s1, s2;
+	cin >> s1 >> s2;
 	memset(dp, -1, sizeof dp);
-	memset(dpTable, 0, sizeof dpTable);
 
-	int coins[n];
-	for (int i = 0; i < n; i++) {
-		cin >> coins[i];
-	}
-
-	cout << coinChangeMinCoins(coins, n, S) << endl;
-	cout << memoized(coins, n, S) << endl;
-	cout << dpPro(coins, n, S) << endl;
+	cout << LCS(s1, s1.length(), s2, s2.length()) << endl;
+	cout << memoized(s1, s1.length(), s2, s2.length()) << endl;
+	cout << dpro(s1, s1.length(), s2, s2.length()) << endl;
 
 	return 0;
 }
