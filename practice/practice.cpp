969e3a506e6ced71de415359113f72fce4af447c
TheHackerOne <yash2200@gmail.com>
2021-05-19 10:14:37 +0530

    update

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,51 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 1e3 + 2;
-int dp[N][N];
-int dpTable[N][N];
 
-int dpro(string s1, int size1, string s2, int size2) {
-	for (int i = 1; i <= size1; i++) {
-		for (int j = 1; j <= size2; j++) {
-			if (s1[i - 1] == s2[j - 1]) dpTable[i][j] = 1 + dpTable[i - 1][j - 1];
-			else dpTable[i][j] = max(dpTable[i - 1][j], dpTable[i][j - 1]);
-		}
-	}
-	int a = size1, b = size2;;
-	while (1) {
-		if (a == 0 || b == 0) break;
-		if (s1[a - 1] == s2[b - 1]) cout << s1[a - 1], a--, b--;
-		else (dpTable[a - 1][b] > dpTable[a][b - 1]) ? a-- : b--;
-	}
-	cout << endl;
-	return dpTable[size1][size2];
+
+int minNumberOfCoins1(int *arr, int size, int sum) {
+    if (sum == 0) return 0;
+    if (size == 0) return INT_MAX;
+
+    if (arr[size - 1] <= sum) {
+        return min(minNumberOfCoins1(arr, size - 1, sum), 1 + minNumberOfCoins1(arr, size, sum - arr[size - 1]));
+    }
+    else {
+        return minNumberOfCoins1(arr, size - 1, sum);
+    }
 }
 
-int memoized(string s1, int size1, string s2, int size2) {
-	if (size1 == 0 || size2 == 0) return 0;
-	if (dp[size1][size2] != -1) return dp[size1][size2];
-	if (s1[size1 - 1] == s2[size2 - 1])
-		return dp[size1][size2] = 1 + memoized(s1, size1 - 1, s2, size2 - 1);
-	else
-		return dp[size1][size2] = max(memoized(s1, size1 - 1, s2, size2), memoized(s1, size1, s2, size2 - 1));
-}
+int minNumberOfCoins(int *arr, int size, int sum) {
+    if (sum == 0) return 0;
+    if (size == 0) return INT_MAX;
 
-int LCS(string s1, int size1, string s2, int size2) {
-	if (size1 == 0 || size2 == 0) return 0;
-	if (s1[size1 - 1] == s2[size2 - 1])
-		return 1 + LCS(s1, size1 - 1, s2, size2 - 1);
-	else
-		return max(LCS(s1, size1 - 1, s2, size2), LCS(s1, size1, s2, size2 - 1));
+    if (arr[size - 1] <= sum) {
+        int res1 = minNumberOfCoins(arr, size - 1, sum);
+        int res2 = minNumberOfCoins(arr, size, sum - arr[size - 1]);
+        return min(res1, 1 + res2);
+    }
+    else {
+        return minNumberOfCoins(arr, size - 1, sum);
+    }
 }
 
 int main() {
-	string s1, s2;
-	cin >> s1 >> s2;
-	memset(dp, -1, sizeof dp);
+    int arr[] = {1, 2, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout << minNumberOfCoins(arr, size, 10) << endl;
+    cout << minNumberOfCoins1(arr, size, 10) << endl;
 
-	cout << LCS(s1, s1.length(), s2, s2.length()) << endl;
-	cout << memoized(s1, s1.length(), s2, s2.length()) << endl;
-	cout << dpro(s1, s1.length(), s2, s2.length()) << endl;
-
-	return 0;
+    return 0;
 }
