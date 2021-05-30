74d3ef4a486e31fd23eb46d2b72932753ef024b7
TheHackerOne <yash2200@gmail.com>
2021-05-30 12:38:55 +0530

    Notes

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,102 +1,60 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class BinaryTreeNode {
+class Node {
 public:
     int data;
-    BinaryTreeNode *left;
-    BinaryTreeNode *right;
+    Node *next;
 
-    BinaryTreeNode(int data) {
+    Node(int data) {
         this -> data = data;
-        left = NULL;
-        right = NULL;
+        this -> next = NULL;
+    }
+
+    ~Node() {
+        delete next;
     }
 };
 
-BinaryTreeNode *TakeInput() {
-    int data;
-    cout << "Enter root data" << endl;
-    cin >> data;
-    BinaryTreeNode *root = new BinaryTreeNode(data);
-    queue<BinaryTreeNode *> pendingNodes;
-    pendingNodes.push(root);
-    while (!pendingNodes.empty()) {
-        BinaryTreeNode *curr = pendingNodes.front();
-        pendingNodes.pop();
-        int left;
-        // cout << "Enter the left child " << endl;
-        cin >> left;
-        if (left != -1) {
-            BinaryTreeNode *newNode = new BinaryTreeNode(left);
-            curr -> left = newNode;
-            pendingNodes.push(newNode);
-        }
-        int right;
-        // cout << "Enter the right child " << endl;
-        cin >> right;
-        if (right != -1) {
-            BinaryTreeNode *newNode = new BinaryTreeNode(right);
-            curr -> right = newNode;
-            pendingNodes.push(newNode);
-        }
+int midElement(Node *head) {
+    Node *slow = head;
+    Node *fast = head;
+    while (fast -> next != NULL && fast -> next -> next != NULL) {
+        slow = slow -> next;
+        fast = fast -> next -> next;
     }
-    return root;
+    return slow -> data;
 }
 
-void printTree(BinaryTreeNode *root) {
-    if (root == NULL)
-        return;
-    cout << root -> data << " : ";
-    if (root -> left != NULL)
-        cout << root -> left -> data << " ";
-    if (root -> right != NULL)
-        cout << root -> right -> data;
+void printLL(Node *head) {
+    while (head != NULL) {
+        cout << head -> data << " ";
+        head = head -> next;
+    }
     cout << endl;
-    printTree(root -> left);
-    printTree(root -> right);
-}
-
-void PrintLevelOrder(BinaryTreeNode *root) {
-    queue<BinaryTreeNode *> pendingNodes;
-    pendingNodes.push(root);
-    while (!pendingNodes.empty()) {
-        BinaryTreeNode *curr = pendingNodes.front();
-        pendingNodes.pop();
-        cout << curr -> data << " : ";
-        if (curr -> left != NULL) {
-            cout << " L" << curr -> left -> data << " ";
-            pendingNodes.push(curr -> left);
-        }
-        if (curr -> right != NULL) {
-            cout << " R" << curr -> right -> data << " ";
-            pendingNodes.push(curr -> right);
-        }
-        cout << endl;
-    }
-}
-
-void printRightView(BinaryTreeNode *root) {
-    queue<BinaryTreeNode *> pendingNodes;
-    pendingNodes.push(root);
-    while (!pendingNodes.empty()) {
-        int size = pendingNodes.size();
-        cout << pendingNodes.front() -> data << endl;
-        while (size--) {
-            BinaryTreeNode *curr = pendingNodes.front();
-            pendingNodes.pop();
-            if (curr -> right != NULL) pendingNodes.push(curr -> right);
-            if (curr -> left != NULL) pendingNodes.push(curr -> left);
-        }
-    }
 }
 
 int main() {
-    BinaryTreeNode *root = TakeInput();
-    // PrintLevelOrder(root);
-    cout << endl;
-    cout << "Right View of Binary tree" << endl;
-    printRightView(root);
+    int n;
+    cin >> n;
+
+    int data;
+    Node *head = NULL;
+    Node *temp = NULL;
+    for (int i = 0; i < n; i++) {
+        cin >> data;
+        Node *newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp -> next = newNode;
+            temp = temp -> next;
+        }
+    }
+
+    printLL(head);
+    cout << "Mid element = " << midElement(head) << endl;
 
     return 0;
 }
