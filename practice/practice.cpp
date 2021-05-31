8e7aba731b8f6f74fbb5226774f58b65d46b2a5d
TheHackerOne <yash2200@gmail.com>
2021-05-31 17:19:56 +0530

    Path sum 3 leet code

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,60 +1,93 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
+class BinaryTreeNode {
 public:
     int data;
-    Node *next;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
 
-    Node(int data) {
+    BinaryTreeNode(int data) {
         this -> data = data;
-        this -> next = NULL;
+        left = right = NULL;
     }
 
-    ~Node() {
-        delete next;
+    ~BinaryTreeNode() {
+        delete left;
+        delete right;
     }
 };
 
-int midElement(Node *head) {
-    Node *slow = head;
-    Node *fast = head;
-    while (fast -> next != NULL && fast -> next -> next != NULL) {
-        slow = slow -> next;
-        fast = fast -> next -> next;
+BinaryTreeNode *TakeInput() {
+    int data;
+    cin >> data;
+    BinaryTreeNode *root = new BinaryTreeNode(data);
+    queue<BinaryTreeNode *> q;
+    q.push(root);
+    while (!q.empty()) {
+        BinaryTreeNode *curr = q.front();
+        q.pop();
+        int left;
+        cout << "Enter the left child of " << curr -> data << endl;
+        cin >> left;
+        if (left != INT_MAX) {
+            BinaryTreeNode *newNode = new BinaryTreeNode(left);
+            curr -> left = newNode;
+            q.push(newNode);
+        }
+        int right;
+        cout << "Enter the right child of " << curr -> data << endl;
+        cin >> right;
+        if (right != INT_MAX) {
+            BinaryTreeNode *newNode = new BinaryTreeNode(right);
+            curr -> right = newNode;
+            q.push(newNode);
+        }
     }
-    return slow -> data;
+    return root;
 }
 
-void printLL(Node *head) {
-    while (head != NULL) {
-        cout << head -> data << " ";
-        head = head -> next;
-    }
-    cout << endl;
+// void printTree(BinaryTreeNode *root) {
+//     queue<BinaryTreeNode *> q;
+//     q.push(root);
+//     while (!q.empty()) {
+//         BinaryTreeNode *curr = q.front();
+//         q.pop();
+//         cout << curr -> data << " : ";
+//         if (curr -> left != NULL) {
+//             cout << "L " << curr -> left -> data << " ";
+//             q.push(curr -> left);
+//         }
+//         if (curr -> right != NULL) {
+//             cout << "R " << curr -> right -> data;
+//             q.push(curr -> right);
+//         }
+//         cout << endl;
+//     }
+// }
+
+map<int, int> freq;
+
+
+int pathSum(BinaryTreeNode *root, int currSum, int targetSum) {
+    if (root == NULL) return 0;
+    int count = 0;
+    currSum += root -> data;
+    if (freq[currSum - targetSum])
+        count++;
+    freq[currSum]++;
+    int p1 = pathSum(root -> left, currSum, targetSum);
+    int p2 = pathSum(root -> right, currSum, targetSum);
+    freq[currSum]--;
+    return p1 + p2 + count;
 }
 
 int main() {
-    int n;
-    cin >> n;
-
-    int data;
-    Node *head = NULL;
-    Node *temp = NULL;
-    for (int i = 0; i < n; i++) {
-        cin >> data;
-        Node *newNode = new Node(data);
-        if (head == NULL) {
-            head = newNode;
-            temp = head;
-        } else {
-            temp -> next = newNode;
-            temp = temp -> next;
-        }
-    }
-
-    printLL(head);
-    cout << "Mid element = " << midElement(head) << endl;
+    int targetSum;
+    cin >> targetSum;
+    BinaryTreeNode *root = TakeInput();
+    freq[0] = 1;
+    cout << "no of halo paths = " << pathSum(root, 0, targetSum) << endl;
 
     return 0;
 }
