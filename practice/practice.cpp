37ce05941f6b1e62aa9e0cd32a5ac8fcdf05e110
TheHackerOne <yash2200@gmail.com>
2021-06-05 19:49:55 +0530

    graph algos

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,90 +1,71 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void buildTree(int *arr, int *tree, int treeNode, int s, int e) {
-    if (s == e) {
-        tree[treeNode] = arr[s];
-        return;
-    }
+int *visited;
 
-    int mid = (s + e) / 2;
-    buildTree(arr, tree, 2 * treeNode, s, mid);
-    buildTree(arr, tree, 2 * treeNode + 1, mid + 1, e);
-    tree[treeNode] = tree[2 * treeNode] + tree[2 * treeNode + 1];
+bool DFS(int src, int parent, vector<int> adjList[]) {
+    visited[src] = 1;
+    for (int child : adjList[src]) {
+        if (!visited[child]) {
+            if (DFS(child, src, adjList))
+                return true;
+        } else if (child != parent)
+            return true;
+
+    }
+    return false;
 }
 
-// We will always update the leaf node which in turn changes the sums in
-// middle nodes and root node
-void updateTree(int *arr, int *tree, int treeNode, int s, int e, int idx, int value) {
-    if (s == e) {
-        arr[idx] = value;
-        tree[treeNode] = value;
-        return;
+void DFS(int src, vector<int> adjList[]) {
+    visited[src] = 1;
+    cout << src << " ";
+    for (int child : adjList[src]) {
+        if (!visited[child]) {
+            DFS(child, adjList);
+        }
     }
-
-    int mid = (s + e) / 2;
-    if (idx > mid) {
-        updateTree(arr, tree, 2 * treeNode + 1, mid + 1, e, idx, value);
-    } else {
-        updateTree(arr, tree, 2 * treeNode, s, mid, idx, value);
-    }
-    tree[treeNode] = tree[2 * treeNode] + tree[2 * treeNode + 1];
 }
 
-int query(int *tree, int treeNode, int s, int e, int left, int right) {
-    // completely outside the given range
-    if (left > e || right < s) {
-        return 0;
+void BFS(int src, vector<int> adjList[]) {
+    queue<int> q;
+    q.push(src);
+    visited[src] = 1;
+    while (!q.empty()) {
+        int curr = q.front();
+        q.pop();
+        cout << curr << " ";
+        for (int child : adjList[curr]) {
+            if (!visited[child]) {
+                visited[child] = 1;
+                q.push(child);
+            }
+        }
     }
-
-    // completely inside the given range
-    if (left <= s && right >= e) {
-        return tree[treeNode];
-    }
-
-    //partially inside and partially outside
-    int mid = (s + e) / 2;
-    int ans1 = query(tree, 2 * treeNode, s, mid, left, right);
-    int ans2 = query(tree, 2 * treeNode + 1, mid + 1, e, left, right);
-    return ans1 + ans2;
-}
-
-void printArray(int *arr, int n) {
-    cout << "array ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    cout << endl;
-}
-
-void printTree(int *tree, int n) {
-    cout << "Tree  ";
-    for (int i = 1; i < 2 * n; i++)
-        cout << tree[i] << " ";
-    cout << endl;
 }
 
 int main() {
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    visited = new int[n + 1];
+    for (int i = 1; i <= n; i++)
+        visited[i] = 0;
+
+    vector<int> adjList[n + 1];
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adjList[a].push_back(b);
+        adjList[b].push_back(a);
     }
 
-    int tree[2 * n];
-    buildTree(arr, tree, 1, 0, n - 1);
+    BFS(1, adjList);
 
-    printArray(arr, n);
-    printTree(tree, n);
+    // if (DFS(1, -1, adjList))
+    //     cout << "Graph contains a cycle" << endl;
+    // else
+    //     cout << "Graph does not contain a cycle" << endl;
 
-    updateTree(arr, tree, 1, 0, n - 1, 2, 12);
-
-    printArray(arr, n);
-    printTree(tree, n);
-
-    cout << "Query = " << query(tree, 1, 0, n - 1, 0, 3);
 
     return 0;
 }
