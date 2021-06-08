3b61b586b30fdb80b38a6bf2401e75303217d979
TheHackerOne <yash2200@gmail.com>
2021-06-08 17:43:41 +0530

    reverse LL in Range

---
+++ 8. Linked List/S - Reverse LL in range.cpp
@@ -0,0 +1,93 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node *next;
+
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void printll(Node *head) {
+    Node *th = head;
+    while (th != NULL) {
+        cout << th -> data << " ";
+        th = th -> next;
+    }
+    cout << endl;
+}
+
+Node *reversell(Node *head) {
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *forw = NULL;
+    while (curr != NULL) {
+        forw = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forw;
+    }
+    return prev;
+}
+
+Node *reverseInRange(Node *head, int s, int e) {
+    if (head == NULL || head -> next == NULL) return head;
+
+    int init = s;
+    int count = 1;
+    Node *rh = head;
+    while (count < s - 1) {
+        rh = rh -> next;
+        count++;
+    }
+    Node *it = rh;
+    rh = rh -> next;
+    Node *rt = rh;
+    init = e - s, count = 0;
+    while (count < init) {
+        rt = rt -> next;
+        count++;
+    }
+    Node *eh = rt -> next;
+    it -> next = NULL;
+    rt -> next = NULL;
+    rt = rh;
+    rh = reversell(rh);
+    it -> next = rh;
+    rt ->next = eh;
+    return head;
+}
+
+int main() {
+    int nodes;
+    cin >> nodes;
+
+    int data;
+    Node *head = NULL;
+    Node *temp = NULL;
+
+    for (int i = 1; i <= nodes; i++) {
+        cin >> data;
+        Node *newNode = new Node(data);
+        if (head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp -> next = newNode;
+            temp = temp -> next;
+        }
+    }
+
+    // printll(head);
+
+    Node *nHead = reverseInRange(head, 2, 4);
+
+    printll(nHead);
+
+    return 0;
+}
--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,109 +1,108 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node *next;
-
-    Node(int data) {
-        this -> data = data;
-        this -> next = NULL;
-    }
-};
-
-void printLL(Node *head) {
-    Node *tHead = head;
-    while (tHead != NULL) {
-        cout << tHead -> data << " ";
-        tHead = tHead -> next;
-    }
-}
-
-int countNode(Node *head) {
-    Node *tHead = head;
-    int count = 0;
-    while (tHead != NULL) {
-        tHead = tHead -> next;
-        count++;
-    }
-    return count;
-}
-
-
-int countNodes(Node *head) {
-    Node *thead = head;
-    int count = 0;
-    while (thead != nullptr) {
-        thead = thead -> next;
-        count++;
-    }
-    return count;
-}
-
-Node *oh = nullptr, *ot = nullptr;
-Node *th = nullptr, *tt = nullptr;
-
-void addfirst(Node *node) {
-    if (th == nullptr) {
-        th = node;
-        tt = node;
-    } else {
-        node -> next = th;
-        th = node;
-    }
-}
-
-Node *reverseInKGroup(Node *head, int k)
-{
-    if (head == nullptr || head -> next == nullptr || k == 0)
-        return head;
-    int length = countNodes(head);
-    Node *curr = head;
-    Node *forw = nullptr;
-
-    while (length >= k) {
-        int n = k;
-        while (n--) {
-            forw = curr -> next;
-            curr -> next = nullptr;
-            addfirst(curr);
-            curr = forw;
-        }
-        if (oh == nullptr) {
-            oh = th;
-            ot = tt;
-        } else {
-            ot -> next = th;
-            ot = tt;
-        }
-        tt = nullptr;
-        th = nullptr;
-        length -= k;
-    }
-    ot -> next = curr;
-    return oh;
-}
-
 int main() {
-    int n;
-    cin >> n;
-
-    int data;
-    Node *head = NULL, *temp = NULL;
-    for (int i = 1; i <= n; i++) {
-        cin >> data;
-        Node *newNode = new Node(data);
-        if (head == NULL) {
-            head = newNode;
-            temp = newNode;
-        } else {
-            temp -> next = newNode;
-            temp = temp -> next;
-        }
+    int arr[2];
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
     }
-    printLL(head);
-    cout << endl;
-    Node *nHead = reverseInKGroup(head, 3);
-    printLL(nHead);
+
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << " ";
+
+    return 0;
 }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// class Node {
+// public:
+//     int data;
+//     Node *next;
+
+//     Node(int data) {
+//         this -> data = data;
+//         this -> next = NULL;
+//     }
+// };
+
+// void printll(Node *head) {
+//     Node *th = head;
+//     while (th != NULL) {
+//         cout << th -> data << " ";
+//         th = th -> next;
+//     }
+//     cout << endl;
+// }
+
+// Node *reversell(Node *head) {
+//     Node *prev = NULL;
+//     Node *curr = head;
+//     Node *forw = NULL;
+//     while (curr != NULL) {
+//         forw = curr -> next;
+//         curr -> next = prev;
+//         prev = curr;
+//         curr = forw;
+//     }
+//     return prev;
+// }
+
+// Node *reverseInRange(Node *head, int s, int e) {
+//     if (head == NULL || head -> next == NULL) return head;
+
+//     int init = s;
+//     int count = 1;
+//     Node *rh = head;
+//     while (count < s - 1) {
+//         rh = rh -> next;
+//         count++;
+//     }
+//     Node *it = rh;
+//     rh = rh -> next;
+//     Node *rt = rh;
+//     init = e - s, count = 0;
+//     while (count < init) {
+//         rt = rt -> next;
+//         count++;
+//     }
+//     Node *eh = rt -> next;
+//     it -> next = NULL;
+//     rt -> next = NULL;
+//     rt = rh;
+//     rh = reversell(rh);
+//     it -> next = rh;
+//     rt ->next = eh;
+//     return head;
+// }
+
+// int main() {
+//     int nodes;
+//     cin >> nodes;
+
+//     int data;
+//     Node *head = NULL;
+//     Node *temp = NULL;
+
+//     for (int i = 1; i <= nodes; i++) {
+//         cin >> data;
+//         Node *newNode = new Node(data);
+//         if (head == NULL) {
+//             head = newNode;
+//             temp = newNode;
+//         } else {
+//             temp -> next = newNode;
+//             temp = temp -> next;
+//         }
+//     }
+
+//     // printll(head);
+
+//     Node *nHead = reverseInRange(head, 2, 4);
+
+//     printll(nHead);
+
+//     return 0;
+// }
