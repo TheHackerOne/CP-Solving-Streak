51cb622b19cfd8a23aceb7cfec185e9fb2358392
TheHackerOne <yash2200@gmail.com>
2021-07-16 08:00:43 +0530

    Printing shortest common supersequence

--- 14. Dynamic Programming/12. printing Longest comman subsequence.cpp
+++ 14. Dynamic Programming/12. printing Longest comman subsequence.cpp
@@ -4,6 +4,45 @@ const int N = 1e3 + 2;
 int dp[N][N];
 int dpTable[N][N];
 
+// printing shortest common supersequence
+string lcs(string a, int al, string b, int bl) {
+	int dp[al + 1][bl + 1];
+	for (int i = 0; i <= al; i++) {
+		for (int j = 0; j <= bl; j++) {
+			dp[i][j] = 0;
+		}
+	}
+
+	for (int i = 1; i <= al; i++) {
+		for (int j = 1; j <= bl; j++) {
+			if (a[i - 1] == b[j - 1]) {
+				dp[i][j] = 1 + dp[i - 1][j - 1];
+			} else {
+				dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+			}
+		}
+	}
+	int i = al, j = bl;
+	string ans = "";
+	while (i > 0 && j > 0) {
+		if (a[i - 1] == b[j - 1]) {
+			ans.push_back(a[i - 1]);
+			i--;
+			j--;
+		} else {
+			if (dp[i][j - 1] > dp[i - 1][j]) {
+				ans.push_back(b[j - 1]);
+				j--;
+			} else {
+				ans.push_back(a[i - 1]);
+				i--;
+			}
+		}
+	}
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
 int dpro(string s1, int size1, string s2, int size2) {
 	for (int i = 1; i <= size1; i++) {
 		for (int j = 1; j <= size2; j++) {
--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,56 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int board[11][11];
-
-bool isValid(int r, int c, int n) {
-  for (int i = r - 1; i >= 0; i--) {
-    if (board[i][c]) return false;
+string lcs(string a, int al, string b, int bl) {
+  int dp[al + 1][bl + 1];
+  for (int i = 0; i <= al; i++) {
+    for (int j = 0; j <= bl; j++) {
+      dp[i][j] = 0;
+    }
   }
 
-  for (int i = r - 1, j = c - 1; i >= 0 && j >= 0; i--, j--) {
-    if (board[i][j]) return false;
-  }
-
-  for (int i = r - 1, j = c + 1; i >= 0 && j < n; i--, j++) {
-    if (board[i][j]) return false;
-  }
-
-  return true;
-}
-
-void nQueensHelper(int n, int row) {
-  if (row == n) {
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        cout << board[i][j] << " ";
+  for (int i = 1; i <= al; i++) {
+    for (int j = 1; j <= bl; j++) {
+      if (a[i - 1] == b[j - 1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
       }
-      cout << endl;
-    }
-    cout << endl;
-    return;
-  }
-
-  for (int i = 0; i < n; i++) {
-    if (isValid(row, i, n)) {
-      board[row][i] = 1;
-      nQueensHelper(n, row + 1);
-      board[row][i] = 0;
     }
   }
-}
-
-void nQueens(int n) {
-  memset(board, 0, 11 * 11 * sizeof(int));
-
-  nQueensHelper(n, 0);
+  int i = al, j = bl;
+  string ans = "";
+  while (i > 0 && j > 0) {
+    if (a[i - 1] == b[j - 1]) {
+      ans.push_back(a[i - 1]);
+      i--;
+      j--;
+    } else {
+      if (dp[i][j - 1] > dp[i - 1][j]) {
+        ans.push_back(b[j - 1]);
+        j--;
+      } else {
+        ans.push_back(a[i - 1]);
+        i--;
+      }
+    }
+  }
+  reverse(ans.begin(), ans.end());
+  return ans;
 }
 
 int main() {
-  int n;
-  cin >> n;
+  string a, b;
+  cin >> a >> b;
 
-  nQueens(n);
+  cout << lcs(a, a.length(), b, b.length());
 
   return 0;
 }
