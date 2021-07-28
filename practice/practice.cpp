f5613fefce658d8aa926d5101babb49062e0b2cb
TheHackerOne <yash2200@gmail.com>
2021-07-28 09:06:27 +0530

    Questions on Binary Search

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,134 +1,34 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// class TreeNode {
-// public:
-//     int data;
-//     TreeNode *left;
-//     TreeNode *right;
-
-//     TreeNode(int data) {
-//         this -> data = data;
-//         left = right = NULL;
-//     }
-// };
-
-// class pairNode {
-// public:
-//     int max;
-//     int min;
-//     int size;
-//     bool isBST;
-// };
-
-
-
-// TreeNode* takeInput() {
-//     int data;
-//     cin >> data;
-//     TreeNode *root = new TreeNode(data);
-//     queue<TreeNode *> q;
-//     q.push(root);
-//     while (!q.empty()) {
-//         TreeNode *front = q.front();
-//         q.pop();
-//         int left;
-//         cin >> left;
-//         if (left != -1) {
-//             TreeNode *newNode = new TreeNode(left);
-//             front -> left = newNode;
-//             q.push(newNode);
-//         }
-//         int right;
-//         cin >> right;
-//         if (right != -1) {
-//             TreeNode *newNode = new TreeNode(right);
-//             front -> right = newNode;
-//             q.push(newNode);
-//         }
-//     }
-//     return root;
-// }
-
-// void printTree(TreeNode *root) {
-//     if (root == NULL) return;
-
-//     cout << root -> data << endl;
-
-//     printTree(root -> left);
-//     printTree(root -> right);
-// }
-
-// void PrintTreeClean(TreeNode *root) {
-//     queue<TreeNode *> q;
-//     q.push(root);
-//     while (!q.empty()) {
-//         TreeNode *front = q.front();
-//         q.pop();
-//         cout << front -> data << " : ";
-//         if (front -> left != NULL) {
-//             cout << front -> left -> data << " ";
-//             q.push(front -> left);
-//         }
-//         if (front -> right != NULL) {
-//             cout << front -> right -> data << " ";
-//             q.push(front -> right);
-//         }
-//         cout << endl;
-//     }
-// }
-
-
-
-// pairNode largestBstHelper(TreeNode *root) {
-//     if (root == NULL) {
-//         pairNode ans;
-//         ans.min = INT_MAX;
-//         ans.max = INT_MIN;
-//         ans.size = 0;
-//         ans.isBST = true;
-//         return ans;
-//     }
-
-//     pairNode left = largestBstHelper(root -> left);
-//     pairNode right = largestBstHelper(root -> right);
-
-//     pairNode ans;
-//     ans.isBST = (left.isBST && right.isBST && root -> data < right.min && root -> data > left.max ) ? true : false;
-//     ans.min = min(root -> data, min(left.min, right.min));
-//     ans.max = max(root -> data, max(left.max, right.max));
-//     if (ans.isBST) {
-//         ans.size = left.size + right.size + 1;
-//     } else {
-//         ans.size = max(left.size, right.size);
-//     }
-
-//     return ans;
-// }
-
-
-
-
-// int largestBst(TreeNode *root)
-// {
-//     pairNode ans = largestBstHelper(root);
-//     return ans.size;
-// }
-
-// int main() {
-//     TreeNode *root = takeInput();
-
-//     int ans =  largestBst(root);
-//     cout << ans;
-
-//     return 0;
-// }
-
 #include<bits/stdc++.h>
 using namespace std;
 
+int maximumSumSubarray(int K, vector<int> &Arr , int N) {
+    int i = 0, j = 0, sum = 0, maxSum = INT_MIN;
+    while (j < N) {
+
+        sum += Arr[j];
+
+        int elementCount = j - i + 1;
+
+        if (elementCount < K)  j++;
+        else if (elementCount == K) {
+            maxSum = max(maxSum, sum);
+            sum -= Arr[i];
+            i++;
+            j++;
+        }
+    }
+    return maxSum;
+}
+
 int main() {
-    cout << (1 || 0 || 0 );
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> arr(n + 1);
+
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    cout << maximumSumSubarray(k, arr, n);
 
     return 0;
 }
