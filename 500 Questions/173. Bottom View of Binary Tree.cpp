e5e52918df8fe82c61aa1c21178433d14240c763
TheHackerOne <yash2200@gmail.com>
2021-09-20 18:12:05 +0530

    173. Bottom View of Binary Tree

---
+++ 500 Questions/173. Bottom View of Binary Tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+	int a[2] = {0, 0};
+
+	void widthTree(Node *root, int hl) {
+		if (root == NULL) return;
+		a[1] = max(a[1], hl);
+		a[0] = min(a[0], hl);
+		widthTree(root -> left, hl - 1);
+		widthTree(root -> right, hl + 1);
+	}
+
+	vector<int> bottomView(Node *root) {
+		widthTree(root, 0);
+		int width = a[1] - a[0] + 1;
+		int rootVertLevel = abs(a[0]);
+		vector<int> levels(width, -1);
+		queue<pair<Node *, int>> q;
+		q.push({ root, rootVertLevel });
+		while (!q.empty()) {
+			int size = q.size();
+			while (size--) {
+				pair<Node *, int> curr = q.front();
+				q.pop();
+				levels[curr.second] = curr.first -> data;
+				if (curr.first -> left != NULL) {
+					q.push({ curr.first -> left, curr.second - 1 });
+				}
+				if (curr.first -> right != NULL) {
+					q.push({ curr.first -> right, curr.second + 1 });
+				}
+			}
+		}
+		return levels;
+	}
+};
