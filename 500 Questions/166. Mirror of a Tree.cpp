20e7e9f6be695de199bb47a71a141cf0f76c877f
TheHackerOne <yash2200@gmail.com>
2021-09-20 12:12:03 +0530

    166. Mirror of a Tree

---
+++ 500 Questions/166. Mirror of a Tree.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+  // Function to convert a binary tree into its mirror tree.
+  void mirror(Node* node) {
+    if (node == nullptr) return;
+    mirror(node -> left);
+    mirror(node -> right);
+    swap(node -> left, node -> right);
+  }
+};
