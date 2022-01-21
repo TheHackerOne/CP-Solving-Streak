aa68d3e0aa6fc314383dade496ce6af8657b1d69
TheHackerOne <yash2200@gmail.com>
2022-01-21 13:59:29 +0530

    1828. Queries on Number of Points Inside a Circle

--- Git Commit.sh
+++ Git Commit.sh
@@ -1,6 +1,9 @@
 
-read -p "Enter Commit Name : " COMMIT
-git add .
-git commit -m "$COMMIT"
-git push -u origin main
-echo "Commit Successful !!!"
+
+while read -p "Enter Commit Name : " COMMIT
+  do    
+    git add .
+    git commit -m "$COMMIT"
+    git push -u origin main
+    echo "Commit Successful !!!"
+done
