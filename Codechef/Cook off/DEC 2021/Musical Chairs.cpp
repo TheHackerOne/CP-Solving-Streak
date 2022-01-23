38531522aa1f70b7a18395ba2b397fcbe3e24a03
TheHackerOne <yash2200@gmail.com>
2022-01-23 22:53:28 +0530

    Median Minimization cook off

--- Codechef/Cook off/DEC 2021/Musical Chairs.cpp
+++ Codechef/Cook off/DEC 2021/Musical Chairs.cpp
@@ -4,13 +4,14 @@ using namespace std;
 void solve() {
     long long n;
     cin>>n;
-    int count = 0;
-    n--;
-    for(int i=1;i*i<=(n);i++){
-        if((n)%i == 0)count += 2;
-        if(i*i == n) count--;
-    }
-    cout<<count<<endl;
+        int count = 0;
+        n--;
+        for(int i=1;i*i<=(n);i++){
+            if((n)%i == 0)count += 2;
+            if(i*i == n) count--;
+        }
+        cout<<count<<endl;
+    
 }
 
 int main() {
