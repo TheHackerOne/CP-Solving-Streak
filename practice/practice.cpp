935ff90e0eb0eee34fb801d2eaf6f27173aa837b
TheHackerOne <yash2200@gmail.com>
2022-01-25 13:40:21 +0530

    1861. Rotating the Box

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,31 +1,13 @@
-class Solution {
-public:
-    int scoreOfParentheses(string s) {
-        stack<char> st;
-        for(int i=0;i<s.length();i++){
-            if(s[i] == '('){
-                st.push('(');
-            }else if(s[i] == ')'){
-                int num = 0;
-                while(st.top() != '('){
-                    num += (666-(int)(st.top()));
-                    st.pop();
-                }
-                st.pop();
-                if(num!=0){
-                    st.push(666-(2*num));
-                }
-                else st.push(666-1);
-            }
-        }
-        if(st.size() == 1) return 666-st.top();
-        else{
-            int ans = 0;
-            while(!st.empty()){
-                ans += (666-(int)(st.top()));
-                st.pop();
-            }
-            return ans;
-        }
-    }
-};
+# # # 
+# # # 
+# # * 
+. * . 
+# . * 
+. . . 
+
+# # # 
+# # # 
+. # * 
+# * . 
+. . * 
+# . . 
