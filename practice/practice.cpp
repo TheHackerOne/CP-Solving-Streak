4214d700c12f67044662332a437514f36b0134a0
TheHackerOne <yash2200@gmail.com>
2022-02-03 10:07:07 +0530

    belllman ford

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,127 +1,159 @@
 #include<bits/stdc++.h>
-// #include <ext/pb_ds/assoc_container.hpp> // Common file
-// #include <ext/pb_ds/tree_policy.hpp> // Including tree_order_statistics_node_update
-
-// using namespace __gnu_pbds;
 using namespace std;
 
-#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MOD 1000000007
-#define MOD1 998244353
-#define INF 1e18
-#define nline "\n"
-#define pb push_back
-#define ppb pop_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define PI 3.141592653589793238462
-#define set_bits __builtin_popcountll
-#define sz(x) ((int)(x).size())
-#define all(x) (x).begin(), (x).end()
-// COMPLEX CLASS
-#define X real()
-#define Y imag()
-#define dist(a, b) abs(a-b)
-#define angle(x) ((arg(x) * 180) / PI)
-#define pos(p, x, y) (conj(p - x) * (p - y)).Y
-// pos > 0 = left, pos < 0 = right, pos = 0 = on the vector
-
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-typedef complex<lld> C;
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(lld t) {cerr << t;}
-void _print(double t) {cerr << t;}
-void _print(ull t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-// template<class T> using oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
-/*---------------------------------------------------------------------------------------------------------------------------*/
-ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
-ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
-void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
-ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
-ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
-bool revsort(ll a, ll b) {return a > b;}
-void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
-void google(int t) {cout << "Case #" << t << ": ";}
-vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (ll j = (ll(i) * ll(i)); j <= n; j += i)arr[j] = 1;} return vect;}
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
-ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
-void precision(int a) {cout << setprecision(a) << fixed;}
-/*--------------------------------------------------------------------------------------------------------------------------*/
-
-
-void solve() {
-    int n;
-    cin>>n;
-    vector<int>arr(n);
-    unordered_map<int, vector<int>>mp;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        mp[arr[i]].push_back(i);
-    }
-
-    bool flag=false;
-    int maxAns = INT_MIN;
-    for(auto i:mp){
-        int ele = i.first;
-        vector<int> idx = i.second;
-        if(idx.size() >= 2){
-            flag = true;
-            int minDiff = INT_MAX, x, y;
-            for(int j=0;j<idx.size()-1;j++){
-                int fidx = idx[j], sidx = idx[j+1];
-                int diff = sidx-fidx;
-                if(diff < minDiff){
-                    minDiff = diff;
-                    x = fidx, y = sidx;
-                }
+void dijikstra(int src, int v, unordered_map<int, vector<pair<int, int>>> &graph){ 
+    vector<int> dist(v, INT_MAX);
+    dist[src] = 0;
+    priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
+    pq.push({ 0, src });
+    vector<bool> visited(v, 0);
+    // remove mark* work add*
+    while(!pq.empty()){
+        // remove
+        pair<int, int> p = pq.top();
+        pq.pop();
+        int node = p.second;
+        int distTillNow = p.first;
+        // mark
+        if(visited[node]) continue;
+        visited[node] = 1;
+        // work
+        cout<<src<<" -> "<<node<<" @ "<<distTillNow<<endl;
+        // add neighbours
+        for(auto i:graph[node]){
+            if(!visited[i.first] && distTillNow+i.second < dist[i.first]){
+                dist[i.first] = distTillNow+i.second;
+                pq.push({ dist[i.first], i.first });
             }
-            int res = x + (n-y);
-            maxAns = max(maxAns, res);
         }
     }
-    if(!flag) {
-        cout<<-1<<nline;
-        return;
+}
+
+void prims(int src, int v, unordered_map<int, vector<pair<int,int>>> graph){
+    vector<bool> visited(v, 0);
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+    pq.push({ 0, { src, src } });
+    while(!pq.empty()){
+        // remove
+        pair<int, pair<int, int>> p = pq.top();
+        pq.pop();
+        int parent = p.second.first;
+        int node = p.second.second;
+        int cost = p.first;
+        // mark
+        if(visited[node]) continue;
+        visited[node] = 1;
+        // work
+        cout<<parent<<" -> "<<node<<" @ "<<cost<<endl;
+        // add neighbour
+        for(auto neigh:graph[node]){
+            if(!visited[neigh.first]){
+                pq.push({ neigh.second, { node, neigh.first }});
+            }
+        }
     }
-    cout<<maxAns<<nline;
+}
+
+bool comp(pair<int, pair<int, int>> &a, pair<int, pair<int, int>> &b){
+    return a.first < b.first;
+}
+
+vector<int> parent(10000, -1);
+
+int findParent(int node){
+    if(parent[node] < 0) return node;
+
+    return findParent(parent[node]);
+}
+
+void unionNodes(int a, int b, int w){
+    int oa = a, ob = b;
+    a = findParent(a);
+    b = findParent(b);
+    if(a != b){
+        cout<<oa<<" -> "<<ob<<" @ "<<w<<endl;
+        if(a < b){
+            parent[a] += parent[b];
+            parent[b] = a;
+        }else{
+            parent[b] += parent[a];
+            parent[a] = b;
+        }
+    }
+}
+
+void kruskal(int src, unordered_map<int, vector<pair<int, int>>> &graph){
+    vector<pair<int, pair<int, int>>> arr;
+    for(auto i:graph){
+        int node = i.first;
+        vector<pair<int, int>> neigh = i.second;
+        for(auto j:neigh){
+            arr.push_back({ j.second, { node, j.first } });
+        }
+    }
+
+    sort(arr.begin(), arr.end(), comp);
+
+    for(int i=0;i<arr.size();i++){
+        int u = arr[i].second.first, v = arr[i].second.second, w = arr[i].first;
+        unionNodes(u, v, w);
+    }
+}
+
+void bellmanFord(int src, int v, unordered_map<int,vector<pair<int, int>>> &graph){
+    vector<int>dist(v, INT_MAX);
+
+    dist[src] = 0;
+
+    for(int i=0;i<v-1;i++){
+        for(auto i:graph){
+            int u = i.first;
+            vector<pair<int, int>> neigh = i.second;
+            for(auto j:neigh){
+                int weight = j.second, v = j.first;
+                if(dist[u] == INT_MAX) continue;
+                if(dist[u] + weight < dist[v]){
+                    dist[v] = dist[u]+weight;
+                }
+            }
+        }
+    }
+    for(auto i:dist)cout<<i<<" ";
 }
 
 int main() {
-#ifndef ONLINE_JUDGE
+#ifndef ONLINE_JUDGE  
     freopen("Error.txt", "w", stderr);
 #endif
 
-    fastio();
-    ll t;
-    cin >> t;
-    while (t--) {
-        solve();
+    int v, e;
+    cin>>v>>e;
+    unordered_map<int, vector<pair<int, int>>> graph;
+    for(int i=0;i<e;i++){
+        int a,b,w;
+        cin>>a>>b>>w;
+        graph[a].push_back({b, w});
+        graph[b].push_back({a, w});
     }
+
+    // single source shortest path in terms of edge weight
+    cout<<"DIJIKSTRA\n";
+    dijikstra(0, v, graph);
+    cout<<endl;
+
+    // minimum spanning tree
+    cout<<"PRIMS \n";
+    prims(0, v, graph);
+    cout<<endl;
+
+    // KRUSKAL minimum spanning tree
+    cout<<"Kruskal\n";
+    kruskal(0, graph);
+    cout<<endl;
+
+    // bellman ford - shortest path in weighted graph with negative edge weight(not negative weight cycle)
+    bellmanFord(0, v, graph);
+
 
     return 0;
 }
