3f5f29e468967ff9c0b7fd21d228319859ae7a24
TheHackerOne <yash2200@gmail.com>
2022-02-21 13:50:51 +0530

    15. 3Sum

--- 18. Misc/Amazon/Medium/15. 3Sum.cpp
+++ 18. Misc/Amazon/Medium/15. 3Sum.cpp
@@ -8,17 +8,21 @@ public:
         
         for(int i=0;i<n;i++){
          int fix = nums[i];
+         if(i > 0 and nums[i] == nums[i-1]) continue;
          int remSum = -fix;
          int first = i+1, last = n-1;
          while(first < last){
             int sum = nums[first]+nums[last];
             if(sum == remSum){
                ans.push_back({ nums[i], nums[first], nums[last] });
-               first++, last--;
+               while(first < last and nums[first] == nums[first+1] and nums[last] == nums[last-1]){
+                    first++,last--;
+               }
+                first++, last--;
             }else if(sum < remSum){
                first++;
             }else{
-               last++;
+               last--;
             }
          }
         }
@@ -26,4 +30,3 @@ public:
         return ans;
     }
 };
-[-2,-1, 1, 2]
