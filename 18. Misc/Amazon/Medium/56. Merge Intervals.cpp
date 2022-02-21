bdc7bd458cfca3a95cbeda6e5e1a74cd4e83d3f0
TheHackerOne <yash2200@gmail.com>
2022-02-21 19:21:08 +0530

    56. Merge Intervals.cpp

--- 18. Misc/Amazon/Medium/56. Merge Intervals.cpp
+++ 18. Misc/Amazon/Medium/56. Merge Intervals.cpp
@@ -9,9 +9,10 @@ public:
 			if(dq.size() == 0){
 				dq.push_back({ start, end });
 			}else{
-				pair<int, int> top = dq.pop_back();
+				pair<int, int> top = dq.back();
+                dq.pop_back();
 				int tpStart = top.first, tpEnd = top.second;
-				if(tpStart >= start and tpStart <= end){
+				if(tpStart <= start and start <= tpEnd){
 					pair<int, int> ans;
 					ans.first = min(start, tpStart);
 					ans.second = max(end, tpEnd);
