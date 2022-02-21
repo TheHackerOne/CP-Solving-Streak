734d1afa2b8e720e4f89a265d7afaa71f79d8b41
TheHackerOne <yash2200@gmail.com>
2022-02-21 09:42:49 +0530

    238. Product of Array Except Self

--- 18. Misc/Amazon/Medium/238. Product of Array Except Self.cpp
+++ 18. Misc/Amazon/Medium/238. Product of Array Except Self.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-      vector<int> ans;
       int n = nums.size();
+      vector<int> ans(n, 0);
 
       for(int i=0;i<n;i++){
          if(i == 0) ans[i] = nums[i];
@@ -12,7 +12,7 @@ public:
       int right = 1;
 
       for(int i=n-1;i>=0;i--){
-         ans[i] = ans[i-1]*right;
+         ans[i] = (i-1>=0?ans[i-1]:1)*right;
          right *= nums[i];
       }
 
