28d17c367170b7f1420ad42387273cafb453fad9
TheHackerOne <yash2200@gmail.com>
2022-02-21 11:42:34 +0530

    12. Integer to Roman

--- 18. Misc/Amazon/Medium/12. Integer to Roman.cpp
+++ 18. Misc/Amazon/Medium/12. Integer to Roman.cpp
@@ -3,18 +3,21 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<string> sign = { "I", "IV", "V", "IX", "X", "XL" "L", "XC", "C", "CD", "D", "CM", "M" };
+        vector<string> sign = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
         vector<int> value = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
 
         string ans = "";
         int idx = value.size()-1;
 
         while(num != 0){
-         if(num/value[idx] > 0){
+         if((num/value[idx]) > 0){
+             cout<<value[idx]<<endl;
+             cout<<num<<endl;
             int times = num/value[idx];
-            for(nit i=0;i<times;i++){
+            for(int i=0;i<times;i++){
                ans += sign[idx];
-            }
+            }            
+            num = num%value[idx];
          }
          idx--;
         }
