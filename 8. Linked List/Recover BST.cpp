940305d40cfd25f5ef17e605b952c07e3e79800c
TheHackerOne <yash2200@gmail.com>
2022-02-28 09:34:53 +0530

    99. Recover BST.cpp

---
+++ 8. Linked List/Recover BST.cpp
@@ -0,0 +1,56 @@
+void recoverTree(TreeNode *root){
+    // morris traversal
+    TreeNode *a1 = nullptr, *b1 = nullptr, *a2 = nullptr, *b2 = nullptr;
+    TreeNode *prev = nullptr;
+    bool prevExist = false;
+    TreeNode*curr = root;
+    vector<int> inorder;
+    while(curr != nullptr){
+        TreeNode *left = curr->left;
+        if(left == nullptr){
+            if(!prevExist){
+                prev = curr;
+                prevExist =true;
+            }else{
+                if(prev->val > curr ->val and a1 == nullptr){
+                    a1 = prev;
+                    b1 = curr;
+                }else if(prev->val > curr->val and a2 == nullptr){
+                    a2 = prev;
+                    b2 = curr;
+                }
+                prev = curr;
+            }      
+            inorder.push_back(curr->val);      
+            curr = curr->right;
+        }else{
+            TreeNode *rightMostNode = getRightMostNode(left, curr);
+            if(rightMostNode->right == nullptr){
+                rightMostNode->right = curr;
+                curr = curr->left;
+            }else{
+                rightMostNode->right = nullptr;
+                if(!prevExist){
+                    prev = curr;
+                    prevExist =true;
+                }else{
+                    if(prev->val > curr ->val and a1 == nullptr){
+                        a1 = prev;
+                        b1 = curr;
+                    }else if(prev->val > curr->val and a2 == nullptr){
+                        a2 = prev;
+                        b2 = curr;
+                    }
+                    prev = curr;
+                }  
+                inorder.push_back(curr->val);  
+                curr = curr->right;
+            }
+        }
+    }
+    if(a2 == nullptr){
+        swap(a1->val, b1->val);
+    }else{
+        swap(a1->val, b2->val);
+    }
+}
--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,31 +1,78 @@
-class Solution {
-private:
-	void dfs(int node, unordered_map<int, vector<int>> &graph, vector<int> &visited,vector<int> &dfsVisited){
-		visited[node] = 1;
-		dfsVisited[node] = 1;
-		for(auto neigh:graph[node]){
-			if(!visited[neigh]){
-				dfs(neigh, graph, visited, dfsVisited);
-			}
-		}
-		dfsVisited[node] = 0;
-	}
-	
-public:
-	bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-		unordered_map<int, vector<int>> graph;
-		for(auto i:prerequisites){
-			int u = i[0], v = i[1];
-			graph[u].push_back(v);
-		}
+#include <iostream>
+using namespace std;
 
-		// cycle detection
-		vector<int> visited(numCourses, 0);
-		vector<int> dfsVisited(numCourses, 0);
-		for(auto i:graph){
-			if(!visited[i.first]){
-				dfs(i.first, graph, visited, dfsVisited);
-			}
-		}
-	}
+class ListNode
+{
+public:
+    int val = 0;
+    ListNode *next = nullptr;
+
+    ListNode(int val)
+    {
+        this->val = val;
+    }
 };
+
+void printList(ListNode *node)
+{
+    ListNode *curr = node;
+    while (curr != nullptr)
+    {
+        cout << curr->val << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+ListNode *segregate012(ListNode *head){
+    ListNode *zero = new ListNode(-1), *one = new ListNode(-1), *two = new ListNode(-1);
+    ListNode *pervZero = zero, *prevOne = one, *prevTwo = two;
+    ListNode *curr = head;
+    while(curr != nullptr){
+        if(curr->val == 0){
+            pervZero->next = curr;
+            pervZero = curr;
+        }else if(curr->val == 1){
+            prevOne->next = curr;
+            prevOne = curr;
+        }else{
+            prevTwo->next = curr;
+            prevTwo = curr;
+        }
+        curr = curr -> next;
+    }
+    pervZero->next = prevOne->next = prevTwo->next = nullptr;
+    // printList(zero->next);
+    // printList(one->next);
+    // printList(two->next);
+    prevOne->next = two->next;
+    pervZero->next = one->next;
+    return zero->next;
+}
+
+
+
+ListNode *createList(int n)
+{
+    ListNode *dummy = new ListNode(-1);
+    ListNode *prev = dummy;
+    while (n-- > 0)
+    {
+        int val;
+        cin >> val;
+        prev->next = new ListNode(val);
+        prev = prev->next;
+    }
+    return dummy->next;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    ListNode *h1 = createList(n);
+    h1 = segregate012(h1);
+    printList(h1);
+
+    return 0;
+}
