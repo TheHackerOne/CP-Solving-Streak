3379920323afd5080c9cadb8f86cf040afb81f02
TheHackerOne <yash2200@gmail.com>
2022-03-13 09:48:33 +0530

    117. Populating Next Right Pointers in Each Node II

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,18 +1,25 @@
-class Solution {
-public:
-  long long numberOfWeeks(vector<int>& milestones) {
-      priority_queue<int> pq;
-      for(auto i:milestones) pq.push(i);
-
-      int ans = 0;
-
-      while(pq.size() > 1){
-        int first = pq.pop();
-        int second = pq.pop();
-        int diff = first - second;
-        ans += min(first, second)*2;
-        if(diff != 0) pq.push(diff);
+void iterativePrePostInTraversal(Node* node) {
+  stack<pair<int, int>> st;
+  vector<int> inorder, preorder, postorder;
+  st.push({ node->val, 1 });
+  while(!st.empty()){
+    Node *node = st.top()->first;
+    int stage = st.top()->second;
+    if(stage == 1){
+      preorder.push_back(node->val);
+      st.top()->second += 1;
+      if(node -> left != nullptr){
+        st.push({ node->left, 1 });
       }
-      return ans;
+    }else if(stage == 2){
+      inorder.push_back(node->val);
+      st.top()->second += 1;
+      if(node->right != nullptr){
+        st.push({ node->right, 1 });
+      }
+    }else{
+      postorder.push_back(node->val);
+      st.pop();
+    }
   }
-};
+}
