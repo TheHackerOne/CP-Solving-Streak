d74cdf18a1b7c4327f3dba546ede98a7ceaa7ee7
TheHackerOne <yash2200@gmail.com>
2022-03-18 17:56:56 +0530

    863. All Nodes Distance K in Binary Tree

--- Leetcode/863. All Nodes Distance K in Binary Tree.cpp
+++ Leetcode/863. All Nodes Distance K in Binary Tree.cpp
@@ -1,55 +1,47 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 private:
-    bool path(TreeNode *root, TreeNode *target, vector<TreeNode*> &p){
-        // base case
+    vector<int> ans;
+    
+    void kDistNodes(TreeNode *root, int k, TreeNode *block){
+        if(root == nullptr or root == block) return;
+        
+        if(k == 0){
+            ans.push_back(root->val);
+            return;
+        }
+        kDistNodes(root->left, k-1, block);
+        kDistNodes(root->right, k-1, block);
+    }
+    
+    bool distanceK_(TreeNode *root, TreeNode *target, int &k){
         if(root == nullptr) return false;
         
-        // main logic
         if(root == target){
-            p.push_back(root);
+            kDistNodes(root, k, nullptr);
+            k--;
             return true;
         }
         
-        bool leftTree = path(root->left, target, p);
-        bool rightTree = path(root->right, target, p);
-        if(leftTree or rightTree){
-            p.push_back(root);
+        bool left = distanceK_(root->left, target, k);
+        bool right = distanceK_(root->right, target, k);
+        
+        if(left){
+            kDistNodes(root, k, root->left);
+            k--;
+            return true;
+        }
+        if(right){
+            kDistNodes(root, k, root->right);
+            k--;
             return true;
         }
         return false;
     }
-    
-    void nodesKDistanceAway(TreeNode *root, int k, TreeNode *block, vector<int> &ans){
-        // base case
-        if(root == nullptr or root == block) return;
-        
-        // main logic
-        if(k == 0) {
-            ans.push_back(root->val);
-            return;
-        }
-        nodesKDistanceAway(root->left, k-1, block, ans);
-        nodesKDistanceAway(root->right, k-1, block, ans);
-    }
-    
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        vector<TreeNode*> nodeToRootPath;
-        path(root, target, nodeToRootPath);
-        // reverse(nodeToRootPath.begin(), nodeToRootPath.end());
-        vector<int> ans;
-        for(int i = 0;i < nodeToRootPath.size();i++){
-            nodesKDistanceAway(nodeToRootPath[i], k-i, i-1>=0?nodeToRootPath[i-1]:nullptr,ans);
-        }
+        distanceK_(root, target, k);
+        
         return ans;
     }
 };
