dd0e63be67ae792a7640b3cd74ac47036a334987
TheHackerOne <yash2200@gmail.com>
2022-06-21 08:30:20 +0530

    Max Sum In The Configuration

--- practice/practice.cpp
+++ practice/practice.cpp
@@ -1,62 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int lowerBound(vector<int> &arr, int target){
-    int lo = 0, hi = arr.size()-1;
-    int res = -1;
-    while(lo <= hi){
-        int mid = (lo + hi)/2;
-        if(arr[mid] == target){
-            if(mid-1 >= 0 and arr[mid-1] == target) {
-                hi = mid-1;
-            }else{
-                return mid;
-            }
-        }else if(arr[mid] > target){
-            hi = mid-1;
-        }else{
-            lo = mid+1;
-        }
-    }
-
-    return res;
-}
-
-int upperBound(vector<int> &arr, int target){
-    int lo = 0, hi = arr.size()-1;
-    int n = arr.size();
-    int res = -1;
-    while(lo <= hi){
-        int mid = (lo + hi)/2;
-        if(arr[mid] == target){
-            if(mid+1 < n and arr[mid+1] == target) {
-                lo = mid+1;
-            }else{
-                return mid;
-            }
-        }else if(arr[mid] > target){
-            hi = mid-1;
-        }else{
-            lo = mid+1;
-        }
-    }
-
-    return res;
-}
-
 int main(){
     int n;
     cin>>n;
     vector<int> arr(n);
+    int initSum = 0, totalSum = 0;
     for(int i=0;i<n;i++){
         cin>>arr[i];
+        totalSum += arr[i];
+        initSum += (i*arr[i]);
     }
-    int target;
-    cin>>target;
+    int maxSum = initSum;
+    for(int i=n-1;i>=0;i--){
+        initSum += totalSum - (arr[i]*(n));
+        maxSum = max(maxSum, initSum);
+    }
 
-    int lo = lowerBound(arr, target);
-    int hi = upperBound(arr, target);
-    cout<<lo<<" "<<hi<<endl;
+    cout<<maxSum<<endl;
 
     return 0;
 }
